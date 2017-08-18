54cc8850231dc3966961ae96144b061431667880
Philip <phils89144@gmail.com>
2017-08-17 23:10:01 -0700

    second revision

--- app.js
+++ app.js
@@ -25,10 +25,8 @@ app.get("/", function(req, res, next){
   res.render("index", {appType:"Express"})
 })
 
-// THIS RETURNS ALL ITEM BY ID NUMBER
-//METHOD: getURL: /ITEMS/{ID}
-//RESPONSE: {ID: INTEGER}, DESCRIPTION, COST QUANTITIY
-//}
+//A customer gets a list of the current items, their costs,
+//and quantities of those items
 app.get("/items/all", function(req, res, next){
    const sql =  `
     Select * FROM items
@@ -41,29 +39,176 @@ app.get("/items/all", function(req, res, next){
 //METHOD: getURL: /ITEMS/{ID}
 //RESPONSE: {ID: INTEGER}, DESCRIPTION, COST QUANTITIY
 //}
-app.get("/items/:id", function(req, res, next){
-   const sql =  `
-    Select * FROM items id = ?
+// app.get("/items/:id", function(req, res, next){
+//    const sql =  `
+//     Select * FROM items id = ?
+//    `
+//    conn.query(sql, [req.params.id], function(err, results, fields){
+//      res.json(results[0])
+//      if (!err) {
+//        res.json({
+//          success: true,
+//          message: "item was created",
+//          id: results.insertId
+//        })
+//      } else {
+//          console.log(err + " return 1 item by id number")
+//          res.json({
+//            success: false,
+//            data:(results),
+//            message: "did not return 1 item by id number",
+//          })
+//        }
+//    })
+// })
+
+//A customer should be able to buy an item using money
+//Making a purchase nd get correct change///
+
+app.post("/items/money/:itemid", function(req, res, next){
+  const itemid1 = req.params.itemid
+
+  const sql0= `
+      SELECT (50 - cost) AS sum
+      from items
+      where id = ?
+  `
+
+  conn.query(sql0, [req.params.itemid], function(err, results, fields){
+    if (err) {
+      res.json({
+        status: "fail",
+        data:(err),
+        message: "Please add additional change",
+      })
+
+    } else{
+  conn.query(sql0, [req.params.itemid], function(err, results, fields){
+           res.json({
+            status: "success",
+            data:(results),
+            message: "Please take your change",
+
+          })
+      })
+    }
+  })
+})
+
+//Customer purchasing an item
+app.post("/items/purch/:itemid", function(req, res, next){
+  const itemid = req.params.itemid
+
+//A customer should not be able to buy items that are not in the machine
+// but instead get an error
+//verify the item is in the machine, if the item is not
+//in the machine return a message to purchase something else
+
+  const sql = `
+      SELECT * FROM items
+      WHERE quantity = 0 and id = ?
+  `
+  conn.query(sql, [req.params.itemid], function(err, results, fields){
+    if (err || results.length > 0){
+      res.json({
+        status: "fail",
+        data:(results),
+        message: "Currently your item is out of stock, please make a different purchase",
+      })
+    } else {
+//Item is in the machine:
+//subtract 1 from quantity and add 1 to the purchase column
+   const sql1 =  `
+      UPDATE items
+      SET quantity = quantity - 1,
+          purchased = purchased + 1
+      WHERE id = ?
    `
-   conn.query(sql, [req.params.id], function(err, results, fields){
-     res.json(results[0])
-     if (!err) {
+   conn.query(sql1, [req.params.itemid], function(err, results, fields){
+     if (err){
+       console.log(err + " sql1")
        res.json({
-         success: true,
-         message: "item was created",
-         id: results.insertId
+         status: "fail",
+         data:(results),
+         message: "purchase was not completed",
        })
      } else {
-         console.log(err + " return 1 item by id number")
-         res.json({
-           success: false,
-           message: "did not return 1 item by id number",
-         })
-       }
+//Insert the purchased item into the purchase table
+  const sql2 = `
+    INSERT INTO purchases (itemid)
+    VALUES (?)
+  `
+  conn.query(sql2, [itemid], function (err, results, fields) {
+      res.json({
+        status: "success",
+        data:(results),
+        message: "purchased item was placed in the purchased table",
+            })
+          })
+        }
+      })
+    }
+  })
+})
+//A vendor should be able to see total amount of money in machine
+app.get("/purchased/total", function(req, res, next){
+   const sql =  `
+    SELECT SUM(purchased * cost) AS sum
+    FROM items
+   `
+   conn.query(sql, [req.params.id], function(err, results, fields){
+     res.json(results)
    })
 })
+
+//A vendor should be able to see a list of all purchases
+//with their time of purchase
+app.get("/purchases/all", function(req, res, next){
+   const sql =  `
+    Select * FROM purchases
+   `
+   conn.query(sql, [req.params.id], function(err, results, fields){
+     res.json(results)
+   })
+})
+
+//A vendor should be able to update the description,
+//quantity, and costs of items in the machine
+
+//This will update an existing item in the item table
+app.put("/items/:id", function(req, res, next){
+  const description = req.body.description
+  const cost = req.body.cost
+  const quantity = req.body.quantity
+
+  const sql = `
+    UPDATE items
+    SET description = ?, cost = ?, quantity = ?
+    WHERE id = ?
+  `
+  conn.query(sql, [description, cost, quantity, req.params.id], function (err, results, fields){
+    if (!err) {
+      res.json({
+        status: "success",
+        message: "item was updated",
+        description:  description,
+        cost: cost,
+        quantity: quantity,
+        id: results.insertId
+      })
+    } else {
+        console.log(err + " update of an existing record")
+        res.json({
+          status: "fail",
+          data: (results),
+          message: "update of an existing record did not occur",
+        })
+    }
+  })
+})
+
+//A vendor should be able to add a new item to the machine
 //Add a new item in the vending machine
-// description
 app.post("/items/add", function(req, res, next){
   const description = req.body.description
   const cost = req.body.cost
@@ -76,75 +221,17 @@ app.post("/items/add", function(req, res, next){
   conn.query(sql, [description, cost, quantity], function (err, results, fields){
     if (!err) {
       res.json({
-        success: true,
-        message: "item was created",
+        status: "success",
+        message: "item was added to the vending machine",
         id: results.insertId
       })
     } else {
         console.log(err + " add a new item in the vending machine")
         res.json({
-          success: false,
+          status: "fail",
+          data:(results),
           message: " new item was not created",
         })
-    }
-  })
-})
-//This will update an existing record
-app.put("/items/:id", function(req, res, next){
-  const description = req.body.description
-  const cost = req.body.cost
-  const quantity = req.body.quantity
-
-  const sql = `
-    UPDATE items
-    SET description = ?, cost = ?, quantity = ?
-    WHERE ID = ?
-  `
-  conn.query(sql, [description, cost, quantity], function (err, results, fields){
-    if (!err) {
-      res.json({
-        success: true,
-        message: "item was created",
-        id: results.insertId
-      })
-    } else {
-        console.log(err + " update of an existing record")
-        res.json({
-          success: false,
-          message: "update of an existing record did not occur",
-        })
-    }
-  })
-})
-
-//Add a purchased item into the purchased table of the vending machine
-app.post("/item/purch/:itemid", function(req, res, next){
-  const itemid = req.params.itemid
-
-   const sql =  `
-   SELECT * FROM Items
-      where quantity > 0 and id = ?
-   `
-   conn.query(sql, [req.params.itemid], function(err, results, fields){
-     if (err){
-       console.log(err + " sql1")
-       res.json({
-         success: false,
-         message: "purchase was not created",
-       })
-     } else {
-  const sql2 = `
-    INSERT INTO purchases (itemid)
-    VALUES (?)
-  `
-  conn.query(sql2, [itemid], function (err, results, fields) {
-       console.log(err + " sql2" , results, fields)
-      res.json({
-        success: true,
-        message: "purchased item was created",
-      //  id: results.insertId
-        })
-      })
     }
   })
 })
